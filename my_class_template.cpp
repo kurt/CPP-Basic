ff76dcca7f176eb5fc61059524904389f83912c4
kurt_stewart <kurt@humaninmotion.ca>
2020-06-23 16:52:30 -0700

    Added a configurable class type template

--- my_class_template.cpp
+++ my_class_template.cpp
@@ -11,9 +11,22 @@ class Array{
     int my_Array[N];
  public: 
     int GetSize() const {return N;}
-};//end of array class
+};//end of Array class
+
+
+template<typename T, int N>
+class cust_Array{
+ private:
+    T my_array[N];
+ public:
+    int GetSize() const {return N;}
+    //char* GetType() const {return typeid(my_Array).name()}
+};//end of cust_Array class
 
 int main(){
     Array<5> array;
     cout << array.GetSize() << endl;
+    cust_Array<float,2> array2;
+    cout << array2.GetSize() << endl;
+    //cout << array2.GetType() << endl;
 }
