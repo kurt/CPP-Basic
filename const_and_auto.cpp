48ddd12a611661b6ae31681b0dc920076d8a28c5
kurt_stewart <kurt@humaninmotion.ca>
2020-06-24 11:21:50 -0700

    Added Const and Auto example

---
+++ const_and_auto.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Entity{
+ private:
+    int x;
+ public:
+    int GetX() const{
+        return x;
+    }
+
+    void SetX(int x_temp){
+        x=x_temp;
+    }
+
+};//end of clas Entity
+
+void PrintEntity(const Entity& e){
+    cout << e.GetX() << endl;
+}
+
+int main(){
+    int random_num=5;
+    int multiplier=2;
+    auto res = random_num*multiplier;
+    const int Max = 10;
+
+    Entity my_entity;
+    my_entity.SetX(10);
+    PrintEntity(my_entity);
+}//end of main
