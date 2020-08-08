d6eabf347bb4d6906a296e72af9235b715b351fe
kurt <stewart.kurt2@gmail.com>
2020-08-07 17:31:14 -0700

    added Cpp course folder and git commands

---
+++ CPP-Udacity/math_operations.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+
+int main(){
+    int base =4;
+	int exponent=2;
+    float answer = std::pow(base,exponent);
+    std::cout << answer << "\n";
+	std::cout << M_PI << "\n";    //pi  
+}
