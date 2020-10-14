44c327c8256a677155e384218f256a13abb248fe
Kurt <stewart.kurt2@gmail.com>
2020-10-14 01:49:46 -0700

    added functions without parameters

--- private_unique_ptr.cpp
+++ private_unique_ptr.cpp
@@ -2,6 +2,9 @@
 #include <memory>
 #include <thread>
 #include <vector>
+#include <chrono>   
+
+int thread_count = 0;
 
 using namespace std;
 class ParentEntity{
@@ -22,7 +25,13 @@ class Entity{
        cout << "do nothing" << "\n";
        m_model->function_call();
    }
+   void function2(){
+       cout << "do something" << "\n";
+   }
    void runAThread();
+   void runOtherThread();
+   std::vector<std::thread> thread1vec;
+   std::vector<std::thread> thread2vec;
    private:
    unique_ptr<ParentEntity> m_model;
 
@@ -34,14 +43,29 @@ void this_func(Entity *e){
 }
 
 void Entity::runAThread(){
-   std::thread thread1(&Entity::function, this, 1); 
-   thread1.join();
+   thread1vec.push_back(std::thread(&Entity::function, this, 1));
+   //std::thread thread1(&Entity::function, this, 1); 
+   thread1vec.at(thread_count).join();
+   thread_count--;
+}
+
+void Entity::runOtherThread(){
+   thread2vec.push_back(std::thread(&Entity::function2,this));
+   thread2vec.at(0).join();
 }
 
 int main(){
 
+
+//for (int i    
+  // if (){
+
+
+  // }
    Entity e;
-   e.runAThread();
+   e.runOtherThread();
+   // Entity e;
+   //std::this_thread::sleep_for (std::chrono::seconds(5));
    //this_func(&e);
    //std::thread thread1(&Entity::function, this, 1); 
   // thread1.join(); 
@@ -49,6 +73,9 @@ int main(){
    //thread1vec.push_back(std::thread(this_func,e));
    //thread1vec.at(1).join();
    for (int i=0;i<6;i++){
+       Entity e;
+       e.runAThread();
+       thread_count++;
       //thread1vec.push_back(std::thread(this_func,e));
       //thread1vec.at(i).join();
    }
