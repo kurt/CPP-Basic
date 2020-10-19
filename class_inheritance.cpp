6dfb4ee492d6b5ab9191516ccf72d397639c5deb
Kurt <stewart.kurt2@gmail.com>
2020-10-19 13:35:00 -0700

    added class inheritance to show vars are copied

---
+++ class_inheritance.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+class ParentClass{
+   public:
+   ParentClass() = default;
+   int variable =10;
+   void parent_func(){
+       std::cout << "Parent Function" << "\n";
+   }
+
+};
+
+
+class ChildClass : public ParentClass{
+   public:
+   ChildClass() = default;
+   void child_func(){
+      std::cout << variable << "\n";
+   }
+
+};
+
+
+int main(){
+   ChildClass cobj;
+   cobj.child_func();
+
+
+}
