dea44d83cb1411e3077ccd4058bff2fdb63783f5
kurt <stewart.kurt2@gmail.com>
2022-01-11 16:19:09 -0800

    Add uncrustify to exception code to try it out

--- exception_basic.cpp
+++ exception_basic.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 struct MyException : public exception {
-   const char * what () const throw () {
-      return "C++ Exception";
-   }
+	const char * what () const throw () {
+		return "C++ Exception";
+	}
 };
- 
+
 int main() {
-   try {
-      throw MyException();
-   } catch(MyException& e) {
-      std::cout << "MyException caught" << std::endl;
-      std::cout << e.what() << std::endl;
-   } catch(std::exception& e) {
-      //Other errors
-   }
+	try {
+		throw MyException();
+	} catch(MyException& e) {
+		std::cout << "MyException caught" << std::endl;
+		std::cout << e.what() << std::endl;
+	} catch(std::exception& e) {
+		//Other errors
+	}
 }
